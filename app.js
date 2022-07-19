105844076286607e06c456a9513ee7db332dea87
bosunabujade <bosunabujade@gmail.com>
2022-07-19 12:38:41 -0400

    Added the css for the to do items and the corresponding buttons

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function addTodo(event) {
     todoDiv.classList.add("todo");
     // Creating Li
     const newTodo = document.createElement("li");
-    newTodo.innerText = "hey"
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
     // Check Mark Button
@@ -31,4 +31,6 @@ function addTodo(event) {
     todoDiv.appendChild(trashButton);
     //Append to List
     todoList.appendChild(todoDiv);
+    // Clear todo input value
+    todoInput.value = ""
 }
